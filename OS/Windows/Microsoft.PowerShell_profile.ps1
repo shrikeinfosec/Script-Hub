0d6073b56093860d0ab67bb2618a536b1f80b38c
shrikeinfosec <shrike@ashcorp.dev>
2022-06-09 11:19:34 +0100

    Added personal PowerShell profile file.

---
+++ OS/Windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,37 @@
+# Dracula Colour Scheme`
+Set-PSReadlineOption -Color @{
+    "Command" = [ConsoleColor]::Green
+    "Parameter" = [ConsoleColor]::Gray
+    "Operator" = [ConsoleColor]::Magenta
+    "Variable" = [ConsoleColor]::White
+    "String" = [ConsoleColor]::Yellow
+    "Number" = [ConsoleColor]::Blue
+    "Type" = [ConsoleColor]::Cyan
+    "Comment" = [ConsoleColor]::DarkCyan
+}
+
+# Dracula Prompt Configuration
+Import-Module posh-git
+$GitPromptSettings.DefaultPromptPrefix.Text = "$([char]0x2192) " # arrow unicode symbol
+$GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Green
+$GitPromptSettings.DefaultPromptPath.ForegroundColor =[ConsoleColor]::Cyan
+$GitPromptSettings.DefaultPromptSuffix.Text = "$([char]0x203A) " # chevron unicode symbol
+$GitPromptSettings.DefaultPromptSuffix.ForegroundColor = [ConsoleColor]::Magenta
+
+# Dracula Git Status Configuration
+$GitPromptSettings.BeforeStatus.ForegroundColor = [ConsoleColor]::Blue
+$GitPromptSettings.BranchColor.ForegroundColor = [ConsoleColor]::Blue
+$GitPromptSettings.AfterStatus.ForegroundColor = [ConsoleColor]::Blue
+
+# Add function for accessing Windows Subsystem for Linux with command.
+function Start-WSL() {
+    wsl -d "ubuntu"
+}
+
+Set-Alias -Name wslu -Value Start-WSL
+
+# Overwrite the default Powershell title and replace with this.
+$host.ui.RawUI.WindowTitle = “PowerShell”
+
+# Enable Starship prompt
+Invoke-Expression (&starship init powershell)
