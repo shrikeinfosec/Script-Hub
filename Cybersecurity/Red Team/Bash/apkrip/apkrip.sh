d79791d91d0d21badf17a8558be8a49088525495
shrikeinfosec <shrike@ashcorp.dev>
2022-06-09 10:13:50 +0100

    Fixed strings. Changed bash to use environment instead of /bin/bash.

--- Cybersecurity/Red Team/Bash/apkrip/apkrip.sh
+++ Cybersecurity/Red Team/Bash/apkrip/apkrip.sh
@@ -1,23 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 runAPKTool() {
-    printf "Running APKTool extraction...\n"
+    printf 'Running APKTool extraction...\n'
     apktool_cmd=$(java -jar "$apktool_loc" decode -q -o "$target/apktool/$target" "$file")
     eval "$apktool_cmd"
 }
 
 runAPKLeaks() {
-    printf "Running APKLeaks extraction...\n"
+    printf 'Running APKLeaks extraction...\n'
     apkleaks_cmd=$(apkleaks -f "$file" -o "$target/apkleaks_$target.json" --json -a "--log-level QUIET") # we want to remove extra logging, so we use quiet.
     eval "$apkleaks_cmd"
 }
 
 print_usage() {
-  printf "Usage: ./apkrip.sh -f <file> -d <output_path> -t <target>"
-  printf "Eg.  : ./apkrip.sh -f com.example.app.apk -d /path/to/output -t google"
+  printf 'Usage: ./apkrip.sh -f <file> -d <output_path> -t <target>'
+  printf 'Eg.  : ./apkrip.sh -f com.example.app.apk -d /path/to/output -t google'
 }
 
-apktool_loc="apktool.jar"
+apktool_loc='apktool.jar'
 
 printf "APK RIP Script v1.0.0"
 
@@ -30,14 +30,14 @@ while getopts "f:t:" flag; do
   esac
 done
 
-printf "\nEvaluating APK file...\n"
-printf "%s was selected for analysis\n" "$file"
+printf '\nEvaluating APK file...\n'
+printf '%s was selected for analysis\n' "$file"
 
 runAPKLeaks
 
-printf "Initial analysis complete\n"
+printf 'Initial analysis complete\n'
 
 runAPKTool
 
-printf "APK decompiled and available in apktool\n"
-printf "Process complete! Press enter to exit...\n"
+printf 'APK decompiled and available in apktool\n'
+printf 'Process complete! Press enter to exit...\n'
